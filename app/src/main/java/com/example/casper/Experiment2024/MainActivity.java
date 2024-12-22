e0b97733f69b75a37c0e3d951e5b0ddaed070fff
Fang <14861172+fang_2024@user.noreply.gitee.com>
2024-12-23 00:20:06 +0800

    实验3

--- app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
+++ app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
@@ -1,49 +1,29 @@
 // MainActivity.java
 package com.example.casper.Experiment2024;
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button1;
-    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
+        String str=getString(R.string.app_name);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(R.string.view_name2);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
 
-        // Initially show Fragment1
-        showFragment(new FirstFragment());
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showFragment(new FirstFragment());
-            }
         });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showFragment(new SecondFragment());
-            }
-        });
-    }
-
-    private void showFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, fragment);
-        fragmentTransaction.commit();
     }
 }
