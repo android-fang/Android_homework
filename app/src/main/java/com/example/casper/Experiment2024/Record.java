0e2a0c76f1a4dfb1839fc02708804382959af773
Fang <14861172+fang_2024@user.noreply.gitee.com>
2024-12-24 23:31:52 +0800

    实验7

--- app/src/main/java/com/example/casper/Experiment2024/Record.java
+++ app/src/main/java/com/example/casper/Experiment2024/Record.java
@@ -2,12 +2,14 @@ package com.example.casper.Experiment2024;
 
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.widget.ImageView;
 
 public class Record {
     private String date;
     private Bitmap image;
     private String comments;
+    private Uri imageUri=null;
 
     public Record(String date,Bitmap img,String com){
         this.date=date;
@@ -15,6 +17,15 @@ public class Record {
         this.comments=com;
     }
 
+    public Uri getImageUri()
+    {
+        return imageUri;
+    }
+
+    public void setImageUri(Uri uri)
+    {
+        this.imageUri=uri;
+    }
     public String getDate()
     {
         return date;
@@ -28,5 +39,20 @@ public class Record {
     public Bitmap getImage()
     {
         return image;
+    }
+
+    public void setDate(String str)
+    {
+        date=str;
+    }
+
+    public void setComments(String str)
+    {
+        comments=str;
+    }
+
+    public void setImage(Bitmap bit)
+    {
+        image=bit;
     }
 }
