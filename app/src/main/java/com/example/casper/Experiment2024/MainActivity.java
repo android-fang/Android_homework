669462523988f3d6be7511c64082e06c184713b9
Fang <14861172+fang_2024@user.noreply.gitee.com>
2025-01-04 15:49:14 +0800

    旅刻app作业

--- app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
+++ app/src/main/java/com/example/casper/Experiment2024/MainActivity.java
@@ -18,7 +18,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 
 public class MainActivity extends AppCompatActivity {
-    private String []tabHeaderStrings = {"打卡","天气","地图","钟表","游戏"};
+    private String []tabHeaderStrings = {"打卡","地图","天气","钟表","游戏"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class FragmentAdapter extends FragmentStateAdapter {
-        private static final int NUM_TABS =5;
+        private static final int NUM_TABS =3;
         public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
             super(fragmentManager, lifecycle);
         }
@@ -55,10 +55,10 @@ public class MainActivity extends AppCompatActivity {
                     return new MapFragment();
                 case 2:
                     return new WeatherFragment();
-                case 3:
-                    return new ClockFragment();
-                case 4:
-                    return new GameFragment();
+//                case 3:
+//                    return new ClockFragment();
+//                case 4:
+//                    return new GameFragment();
 
 
                 default:
